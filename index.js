8d6e363e6a8b398c151fc24a58d70db6475c295b
Gabriel Henrique <133382006+gielzzin@users.noreply.github.com>
2024-09-02 13:18:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+for(let contador = 1 ; contador < 6 ; contador++) {
+  console.log(contador);
+}
+
+for (let i = 5; i > 0; i--) {
+  console.log(i)
+}
+
+const prompt = require('prompt-sync');
+const entrada = prompt();
+
+var quantosNumerosSeraoSomados = entrada('Digite aqui quantos números desejam somar: ')
